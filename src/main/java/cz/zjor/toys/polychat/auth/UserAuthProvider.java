da12d1d23dbba70156c787836fb7b0257955a7da
zjor <zjor.se@gmail.com>
2015-11-20 23:49:31 +0100

    spring security was added

---
+++ src/main/java/cz/zjor/toys/polychat/auth/UserAuthProvider.java
@@ -0,0 +1,21 @@
+package cz.zjor.toys.polychat.auth;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service("userAuthProvider")
+public class UserAuthProvider implements AuthenticationProvider {
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        return null;
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return false;
+    }
+}
